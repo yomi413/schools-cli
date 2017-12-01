6dab6e892dfe9cc7736cf0096e907dbeb5dec9dc
github username <github email address>
2017-11-30 23:08:55 -0500

    changed from IRB to PRY in console

---
+++ lib/schools/cli.rb
@@ -0,0 +1,7 @@
+require "schools/cli/version"
+
+module Schools
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ spec/schools/cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Schools::Cli do
+  it "has a version number" do
+    expect(Schools::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
