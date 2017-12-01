f8d08ed0d25d06d7320e0e432d20cf3387bb98e4
github username <github email address>
2017-11-30 23:13:22 -0500

    preliminary code added to CLI class

---
+++ lib/schools/cli/cli.rb
@@ -0,0 +1,8 @@
+class Schools::CLI::CLI
+
+	def list
+		puts "---------- List of Public Middle Schools in Bronx, NY ----------"
+		puts "1. Mott Hall Community School"
+		puts "2. Urban Institute of Mathematics"
+	end
+end
