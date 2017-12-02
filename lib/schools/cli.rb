d50765185ac4ae50425ce27805f08c1080ceb91c
github username <github email address>
2017-12-01 22:29:20 -0500

    added basic code; tested in terminal

--- lib/schools/cli.rb
+++ lib/schools/cli.rb
@@ -5,8 +5,18 @@ class Schools::CLI
 	end
 
 	def list
-		puts "---------- List of Public Middle Schools in Bronx, NY ----------"
+		puts ""
+		puts ""
+		puts "******** List of Public Middle Schools in District 8 of Bronx, NY ********"
+		puts ""
 		puts "1. Mott Hall Community School"
 		puts "2. Urban Institute of Mathematics"
+		puts ""
+		puts ""
+		puts "What school would you like more information about, by number?"
+		puts ""
+
+
 	end
+
 end
