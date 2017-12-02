9c7b593c8291d76a080d41ffb1f58831545362e1
github username <github email address>
2017-12-01 23:51:35 -0500

    generated schools list

--- lib/schools/cli.rb
+++ lib/schools/cli.rb
@@ -9,12 +9,15 @@ class Schools::CLI
 		puts ""
 		puts "******** List of Public Middle Schools in District 8 of Bronx, NY ********"
 		puts ""
-		puts "1. Mott Hall Community School"
-		puts "2. Urban Institute of Mathematics"
+		Schools::Scraper.new.schools_list
 		puts ""
 		puts ""
 		puts "What school would you like more information about, by number?"
 		puts ""
 	end
+
+	# def start
+	# 	list
+	# end
 
 end
--- lib/schools/scraper.rb
+++ lib/schools/scraper.rb
@@ -1,16 +1,19 @@
+require "pry"
+
 class Schools::Scraper
 
 	def get_page
 		doc = Nokogiri::HTML(open("https://insideschools.org/search/results?borough=bronx&district=8&neighborhood=&grade_level=ms&button="))
-		binding.pry
+		# binding.pry
 	end
 
 	def scrape_for_schools
-		self.get_page.css("div.result")
+		get_page.css("div.result")
 	end
 
 	def schools_list
-		self.scrape_for_schools.each.with_index(1) do |school, i|
+		schools_list = self.get_page.css("div.result-info a.result-name").text.split("\n")
+		schools_list.each.with_index(1) do |school, i|
 			puts "#{i}.	#{school}"
 		end
 	end
