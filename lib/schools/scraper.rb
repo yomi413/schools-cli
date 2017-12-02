6c8c3bc6300c259eb677fa632949e073e6e00709
github username <github email address>
2017-12-01 23:16:13 -0500

    set up the scraper class

--- lib/schools/scraper.rb
+++ lib/schools/scraper.rb
@@ -1,7 +1,18 @@
 class Schools::Scraper
 
 	def get_page
-		Nokogiri::HTML(open("https://insideschools.org/search/results?borough=bronx&district=8&neighborhood=&grade_level=ms&button="))
+		doc = Nokogiri::HTML(open("https://insideschools.org/search/results?borough=bronx&district=8&neighborhood=&grade_level=ms&button="))
+		binding.pry
+	end
+
+	def scrape_for_schools
+		self.get_page.css("div.result")
+	end
+
+	def schools_list
+		self.scrape_for_schools.each.with_index(1) do |school, i|
+			puts "#{i}.	#{school}"
+		end
 	end
 
 end
