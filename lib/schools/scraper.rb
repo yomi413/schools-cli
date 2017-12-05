0b65ee5b6c6b703cbae03a4edd9319d8f7930157
github username <github email address>
2017-12-04 23:02:35 -0500

    added #make_schools method

--- lib/schools/scraper.rb
+++ lib/schools/scraper.rb
@@ -11,6 +11,12 @@ class Schools::Scraper
 		get_page.css("div.result")
 	end
 
+	def make_schools
+		self.scrape_for_schools.each do |school|
+			Schools::MiddleSchools.new_from_index_page(school)
+		end
+	end
+
 	def schools_list
 		schools_list = self.get_page.css("div.result-info a.result-name").text.split("\n")
 		schools_list.each.with_index(1) do |school, i|
